56c9e5b291679305fe668c9a2268c8334b38643b
liton2k1 <litonmia2k1@gmail.com>
2026-04-14 03:29:32 +0600

    module 1 done

---
+++ module-1/tab.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Dhaka is capital of\tBangladesh");
+    return 0;
+}
---
+++ module-1/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int a = 10;
+    float b = 10.5;
+    char c = 'C';
+    bool d = true;
+
+    printf("%d\n%.2f\n%c\n%d\n", a, b, c, d);
+
+    return 0;
+}
