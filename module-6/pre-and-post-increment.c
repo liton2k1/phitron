c9379aa1ddb6866d3eae56e3297731e96e86c733
liton2k1 <litonmia2k1@gmail.com>
2026-04-15 22:44:33 +0600

    module 6 added

---
+++ module-6/pre-and-post-increment.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int x = 10;
+//     int y = x++;
+//     int z = ++y;
+
+//     printf("%d\n", z++);
+//     printf("%d", z);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    ++x;
+    printf("%d", ++x);
+    return 0;
+}
