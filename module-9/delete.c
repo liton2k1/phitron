735a23e05b857ad0797bf137f28bf404ff541ff0
liton2k1 <litonmia2k1@gmail.com>
2026-04-20 23:53:02 +0600

    module 9 added

---
+++ module-9/delete.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int idx;
+    scanf("%d", &idx);
+    return 0;
+}
