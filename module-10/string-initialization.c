9f19656f385563610645598e437f6d55ff1aaa26
liton2k1 <litonmia2k1@gmail.com>
2026-04-25 21:48:28 +0600

    module 10 added

---
+++ module-10/string-initialization.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    // char s[6] = {'L', 'I', 'T', 'O', 'N','\0'};
+    char s[10] = "LITON MIA";
+    printf("%s", s);
+    return 0;
+}
--- module-9/delete.c
+++ module-9/delete.c
@@ -10,5 +10,14 @@ int main()
     }
     int idx;
     scanf("%d", &idx);
+    for (int i = idx; i < n - 1; i++)
+    {
+        a[i] = a[i + 1];
+    }
+    n--;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
     return 0;
 }
